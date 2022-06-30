0c5644a54a98de865314deccbaf19a363975f3cc
leopassosleite <leopassos.leite4@gmail.com>
2022-06-29 23:36:55 -0300

    Projeto configurado e criado

---
+++ backend/src/main/java/com/silentsoft/leostagram/LeostagramApplication.java
@@ -0,0 +1,13 @@
+package com.silentsoft.leostagram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeostagramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeostagramApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/silentsoft/leostagram/LeostagramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.silentsoft.leostagram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeostagramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
